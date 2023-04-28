fe835b20f01eb89bf1ef5dab3e2085ccdf224d7d
Luís Lobo <ellobo@ipb.pt>
2023-04-28 10:44:29 +0100

    - prepare challenge

---
+++ src/main/java/pt/ipb/dsys/peer/PeerMain.java
@@ -0,0 +1,15 @@
+package pt.ipb.dsys.peer;
+
+public class PeerMain {
+
+  public static final String CLUTER_NAME = "PeerCluster";
+
+  public static final String GOSSIP_ROUTER_HOSTNAME = "gossip-router";
+
+  private static final int GOSSIP_ROUTER_PORT = 12001;
+
+  public static void main(String[] args) {
+
+  }
+
+}
