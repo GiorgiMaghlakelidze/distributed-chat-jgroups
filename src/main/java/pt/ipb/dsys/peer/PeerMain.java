5dd7466db57ec985cc8a174d0350627fb1fef7a8
Luís Lobo <ellobo@ipb.pt>
2024-05-22 09:06:46 +0100

    fix var name

--- src/main/java/pt/ipb/dsys/peer/PeerMain.java
+++ src/main/java/pt/ipb/dsys/peer/PeerMain.java
@@ -2,7 +2,7 @@ package pt.ipb.dsys.peer;
 
 public class PeerMain {
 
-  public static final String CLUTER_NAME = "PeerCluster";
+  public static final String CLUSTER_NAME = "PeerCluster";
 
   public static final String GOSSIP_ROUTER_HOSTNAME = "127.0.0.1";
 
